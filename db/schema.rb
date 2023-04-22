014c3e2dadccefc3d0516c9e774a1dad2debe69b
YoSoyJaime <jhormazab@uc.cl>
2023-04-22 01:26:20 -0400

    CRUD solicitudes y html con bootstrap

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_22_042939) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_22_045139) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -59,6 +59,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_22_042939) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "description"
+  end
+
+  create_table "requests", force: :cascade do |t|
+    t.integer "user_emit_id"
+    t.integer "user_recive_id"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
