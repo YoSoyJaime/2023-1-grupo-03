c3de81e82e2559772ce988665a00b0f957724dd5
maxewilke <maxeimilian.wilke@uc.cl>
2023-04-26 23:16:44 -0400

    Arreglado2??

--- db/schema.rb
+++ db/schema.rb
@@ -29,8 +29,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_27_030655) do
     t.string "duration"
     t.string "modality"
     t.string "price"
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_publications_on_user_id"
   end
 
   create_table "requests", force: :cascade do |t|
@@ -63,4 +65,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_27_030655) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "publications", "users"
 end
