48d3c0a3a319b8a5aa795a1f1c632479ebc37314
maxewilke <maxeimilian.wilke@uc.cl>
2023-04-26 23:08:23 -0400

    Arreglado?

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_27_025023) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_27_030655) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,10 +29,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_27_025023) do
     t.string "duration"
     t.string "modality"
     t.string "price"
-    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_publications_on_user_id"
   end
 
   create_table "requests", force: :cascade do |t|
@@ -65,5 +63,4 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_27_025023) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "publications", "users"
 end
