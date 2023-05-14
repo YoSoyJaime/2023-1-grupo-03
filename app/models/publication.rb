36d5de955d6fdf8bcc00e286172bb2a537b91a7d
maxewilke <maxeimilian.wilke@uc.cl>
2023-05-13 21:43:38 -0400

    Funciona?

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -1,9 +1,4 @@
 class Publication < ApplicationRecord
     belongs_to :user
-    validates :description, presence: true
-    validates :contents, presence: true
-    validates :duration, presence: true
-    validates :modality, presence: true
-    validates :price, presence: true
-    validates :likes_number, presence: true
+    
 end
--- app/views/publications/_publication.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! publication, :id, :user_id, :class_id, :likes_number, :contents, :description, :duration, :modality, :price, :created_at, :updated_at
-json.url publication_url(publication, format: :json)
